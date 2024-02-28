15ebf1dc072d5e0520a8c20fe6796cba8fae5bac
refayet21 <refayet21@gmail.com>
2024-02-28 16:36:10 +0600

    first commit

---
+++ lib/infrastructure/navigation/bindings/controllers/controllers_bindings.dart
@@ -0,0 +1,2 @@
+export 'package:dpil/infrastructure/navigation/bindings/controllers/login.controller.binding.dart';
+export 'package:dpil/infrastructure/navigation/bindings/controllers/splash.controller.binding.dart';
---
+++ lib/presentation/screens.dart
@@ -0,0 +1,2 @@
+export 'package:dpil/presentation/login/login.screen.dart';
+export 'package:dpil/presentation/splash/splash.screen.dart';
