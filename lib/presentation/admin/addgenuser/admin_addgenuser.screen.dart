593e4b05aa592101b43ecb3904f5890541b55495
refayet21 <refayet21@gmail.com>
2024-02-29 10:37:40 +0600

    general user added successfully

--- lib/presentation/admin/addgenuser/admin_addgenuser.screen.dart
+++ lib/presentation/admin/addgenuser/admin_addgenuser.screen.dart
@@ -1,5 +1,6 @@
 import 'package:dpil/presentation/widgets/admin_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
@@ -7,20 +8,288 @@ import 'controllers/admin_addgenuser.controller.dart';
 
 class AdminAddgenuserScreen extends GetView<AdminAddgenuserController> {
   const AdminAddgenuserScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: AdminDrawer(),
       appBar: AppBar(
-        title: Text('DPIL'),
+        title: Text(
+          'DPIL',
+          style: TextStyle(fontSize: 30.sp, fontWeight: FontWeight.w700),
+        ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'AdminAddgenuserScreen is working',
-          style: TextStyle(fontSize: 20),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) => controller.searchgeneraluser(value),
+              decoration: InputDecoration(
+                hintText: "Search",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          Expanded(
+            child: Obx(
+              () => ListView.builder(
+                itemCount: controller.foundgeneraluser.length,
+                itemBuilder: (context, index) => Card(
+                  color: Colors.grey.shade200,
+                  child: ListTile(
+                    title: Text(
+                      'Name : ${controller.foundgeneraluser[index].name!}',
+                      style: TextStyle(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                    ),
+                    subtitle: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 3.h,
+                        ),
+                        Text(
+                          'Address :${controller.foundgeneraluser[index].address!}',
+                          style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                        SizedBox(
+                          height: 3.h,
+                        ),
+                        Text(
+                          'Mobile : ${controller.foundgeneraluser[index].mobile!}',
+                          style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                        SizedBox(
+                          height: 3.h,
+                        ),
+                        Text(
+                          'Email : ${controller.foundgeneraluser[index].email!}',
+                          style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                        SizedBox(
+                          height: 3.h,
+                        ),
+                        Text(
+                          'Password : ${controller.foundgeneraluser[index].password!}',
+                          style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    leading: CircleAvatar(
+                      child: Text(
+                        controller.foundgeneraluser[index].name!
+                            .substring(0, 1)
+                            .capitalize!,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, color: Colors.black),
+                      ),
+                      backgroundColor: Colors.blue.shade200,
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.delete_forever,
+                        color: Colors.red,
+                      ),
+                      onPressed: () {
+                        displayDeleteDialog(
+                            controller.foundgeneraluser[index].docId!);
+                      },
+                    ),
+                    onTap: () {
+                      controller.nameController.text =
+                          controller.foundgeneraluser[index].name!;
+                      controller.addressController.text =
+                          controller.foundgeneraluser[index].address!;
+                      controller.mobileController.text =
+                          controller.foundgeneraluser[index].mobile!;
+                      controller.emailController.text =
+                          controller.foundgeneraluser[index].email!;
+                      controller.passwordController.text =
+                          controller.foundgeneraluser[index].password!;
+
+                      _buildAddEditGeneralUserView(
+                          text: 'UPDATE',
+                          addEditFlag: 2,
+                          docId: controller.foundgeneraluser[index].docId!);
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: ElevatedButton(
+          onPressed: () {
+            _buildAddEditGeneralUserView(
+                text: 'ADD', addEditFlag: 1, docId: '');
+          },
+          child: Text('Add General User')),
+    );
+  }
+
+  _buildAddEditGeneralUserView(
+      {String? text, int? addEditFlag, String? docId}) {
+    Get.bottomSheet(
+      Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(16.r),
+              topLeft: Radius.circular(16.r),
+            ),
+            color: Colors.blue.shade200),
+        child: Padding(
+          padding:
+              EdgeInsets.only(left: 16.w, right: 16.w, top: 16.h, bottom: 16.h),
+          child: Form(
+            key: controller.formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${text} General User',
+                    style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                    ),
+                    controller: controller.nameController,
+                    // validator: (value) {
+                    //   return controller.validateName(value!);
+                    // },
+                  ),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      hintText: 'address',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                    ),
+                    controller: controller.addressController,
+                  ),
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      hintText: 'Mobile',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                    ),
+                    controller: controller.mobileController,
+                  ),
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                    ),
+                    controller: controller.emailController,
+                  ),
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                    ),
+                    controller: controller.passwordController,
+                  ),
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints.tightFor(
+                        width: Get.context!.width, height: 45.h),
+                    child: ElevatedButton(
+                      child: Text(
+                        text!,
+                        style: TextStyle(color: Colors.black, fontSize: 16.sp),
+                      ),
+                      onPressed: () {
+                        controller.saveUpdategeneralUsers(
+                          controller.nameController.text,
+                          controller.addressController.text,
+                          controller.mobileController.text,
+                          controller.emailController.text,
+                          controller.passwordController.text,
+                          docId!,
+                          addEditFlag!,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
+    );
+  }
+
+  displayDeleteDialog(String docId) {
+    Get.defaultDialog(
+      title: "Delete General User",
+      titleStyle: TextStyle(fontSize: 20.sp),
+      middleText: 'Are you sure to delete General User ?',
+      textCancel: "Cancel",
+      textConfirm: "Confirm",
+      confirmTextColor: Colors.black,
+      onCancel: () {},
+      onConfirm: () {
+        controller.deleteData(docId);
+      },
     );
   }
 }
--- lib/presentation/admin/addgenuser/controllers/admin_addgenuser.controller.dart
+++ lib/presentation/admin/addgenuser/controllers/admin_addgenuser.controller.dart
@@ -1,12 +1,127 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dpil/model/general_user_model.dart';
+import 'package:dpil/presentation/widgets/customFullScreenDialog.dart';
+import 'package:dpil/presentation/widgets/customSnackBar.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AdminAddgenuserController extends GetxController {
-  //TODO: Implement AdminAddgenuserController
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  late TextEditingController nameController,
+      addressController,
+      mobileController,
+      emailController,
+      passwordController;
+  RxList<GeneralUserModel> foundgeneraluser = RxList<GeneralUserModel>([]);
 
-  final count = 0.obs;
+  // Firestore operation
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+  late CollectionReference collectionReference;
+
+  RxList<GeneralUserModel> generalusers = RxList<GeneralUserModel>([]);
+
   @override
   void onInit() {
     super.onInit();
+    nameController = TextEditingController();
+    addressController = TextEditingController();
+    mobileController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    collectionReference = firebaseFirestore.collection("general_users");
+    generalusers.bindStream(getAllgeneralUsers());
+    foundgeneraluser = generalusers;
+  }
+
+  String? validateName(String value) {
+    if (value.isEmpty) {
+      return "Name can not be empty";
+    }
+    return null;
+  }
+
+  String? validateaddress(String value) {
+    if (value.isEmpty) {
+      return "address can not be empty";
+    }
+    return null;
+  }
+
+  String? validatemobile(String value) {
+    if (value.isEmpty) {
+      return "mobile can not be empty";
+    }
+    return null;
+  }
+
+  void saveUpdategeneralUsers(
+    String? name,
+    String? address,
+    String? mobile,
+    String? email,
+    String? password,
+    String? docId,
+    int? addEditFlag,
+  ) {
+    final isValid = formKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    formKey.currentState!.save();
+    if (addEditFlag == 1) {
+      CustomFullScreenDialog.showDialog();
+      collectionReference.add({
+        'name': name,
+        'address': address,
+        'mobile': mobile,
+        'email': email,
+        'password': password,
+      }).whenComplete(() {
+        CustomFullScreenDialog.cancelDialog();
+        clearEditingControllers();
+        Get.back();
+        CustomSnackBar.showSnackBar(
+            context: Get.context,
+            title: "General User Added",
+            message: "General User added successfully",
+            backgroundColor: Colors.green);
+        // ignore: body_might_complete_normally_catch_error
+      }).catchError((error) {
+        CustomFullScreenDialog.cancelDialog();
+        CustomSnackBar.showSnackBar(
+            context: Get.context,
+            title: "Error",
+            message: "Something went wrong",
+            backgroundColor: Colors.green);
+      });
+    } else if (addEditFlag == 2) {
+      //update
+      CustomFullScreenDialog.showDialog();
+      collectionReference.doc(docId).update({
+        'name': name,
+        'address': address,
+        'mobile': mobile,
+        'email': email,
+        'password': password,
+      }).whenComplete(() {
+        CustomFullScreenDialog.cancelDialog();
+        clearEditingControllers();
+        Get.back();
+        CustomSnackBar.showSnackBar(
+            context: Get.context,
+            title: "General User Updated",
+            message: "General User updated successfully",
+            backgroundColor: Colors.green);
+      }).catchError((error) {
+        CustomFullScreenDialog.cancelDialog();
+        CustomSnackBar.showSnackBar(
+            context: Get.context,
+            title: "Error",
+            message: "Something went wrong",
+            backgroundColor: Colors.red);
+      });
+    }
   }
 
   @override
@@ -16,8 +131,54 @@ class AdminAddgenuserController extends GetxController {
 
   @override
   void onClose() {
-    super.onClose();
+    nameController.dispose();
+    addressController.dispose();
+    mobileController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
   }
 
-  void increment() => count.value++;
+  void clearEditingControllers() {
+    nameController.clear();
+    addressController.clear();
+    mobileController.clear();
+    emailController.clear();
+    passwordController.clear();
+  }
+
+  Stream<List<GeneralUserModel>> getAllgeneralUsers() =>
+      collectionReference.snapshots().map((query) =>
+          query.docs.map((item) => GeneralUserModel.fromJson(item)).toList());
+
+  void deleteData(String docId) {
+    CustomFullScreenDialog.showDialog();
+    collectionReference.doc(docId).delete().whenComplete(() {
+      CustomFullScreenDialog.cancelDialog();
+      Get.back();
+      CustomSnackBar.showSnackBar(
+          context: Get.context,
+          title: "General User Deleted",
+          message: "General User deleted successfully",
+          backgroundColor: Colors.green);
+    }).catchError((error) {
+      CustomFullScreenDialog.cancelDialog();
+      CustomSnackBar.showSnackBar(
+          context: Get.context,
+          title: "Error",
+          message: "Something went wrong",
+          backgroundColor: Colors.red);
+    });
+  }
+
+  void searchgeneraluser(String searchQuery) {
+    if (searchQuery.isEmpty) {
+      foundgeneraluser.assignAll(generalusers.toList());
+    } else {
+      List<GeneralUserModel> results = generalusers
+          .where((element) =>
+              element.name!.toLowerCase().contains(searchQuery.toLowerCase()))
+          .toList();
+      foundgeneraluser.assignAll(results);
+    }
+  }
 }
