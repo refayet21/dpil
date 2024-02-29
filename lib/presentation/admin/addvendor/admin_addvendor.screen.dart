d48639c2cb68edcad593028c080ab4474ab62df9
refayet21 <refayet21@gmail.com>
2024-02-29 08:04:40 +0600

    vendor added using firebase

--- lib/presentation/admin/addvendor/admin_addvendor.screen.dart
+++ lib/presentation/admin/addvendor/admin_addvendor.screen.dart
@@ -1,5 +1,6 @@
 import 'package:dpil/presentation/widgets/admin_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
@@ -7,20 +8,238 @@ import 'controllers/admin_addvendor.controller.dart';
 
 class AdminAddvendorScreen extends GetView<AdminAddvendorController> {
   AdminAddvendorScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: AdminDrawer(),
       appBar: AppBar(
-        title: Text('DPIL'),
+        // leading: Icon(
+        //   Icons.medical_information,
+        //   size: 30.sp,
+        //   color: Colors.black,
+        // ),
+        title: Text(
+          'DPIL',
+          style: TextStyle(fontSize: 30.sp, fontWeight: FontWeight.w700),
+        ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'AdminAddvendorScreen is working',
-          style: TextStyle(fontSize: 20),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) => controller.searchVendor(value),
+              decoration: InputDecoration(
+                hintText: "Search",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          Expanded(
+            child: Obx(
+              () => ListView.builder(
+                itemCount: controller.foundVendor.length,
+                itemBuilder: (context, index) => Card(
+                  color: Colors.grey.shade200,
+                  child: ListTile(
+                    title: Text(
+                      'Name : ${controller.foundVendor[index].name!}',
+                      style: TextStyle(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                    ),
+                    subtitle: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 3.h,
+                        ),
+                        Text(
+                          'Address :${controller.foundVendor[index].address!}',
+                          style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                        SizedBox(
+                          height: 3.h,
+                        ),
+                        Text(
+                          'Mobile : ${controller.foundVendor[index].mobile!}',
+                          style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    leading: CircleAvatar(
+                      child: Text(
+                        controller.foundVendor[index].name!
+                            .substring(0, 1)
+                            .capitalize!,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, color: Colors.black),
+                      ),
+                      backgroundColor: Colors.blue.shade200,
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.delete_forever,
+                        color: Colors.red,
+                      ),
+                      onPressed: () {
+                        displayDeleteDialog(
+                            controller.foundVendor[index].docId!);
+                      },
+                    ),
+                    onTap: () {
+                      controller.nameController.text =
+                          controller.foundVendor[index].name!;
+                      controller.addressController.text =
+                          controller.foundVendor[index].address!;
+                      controller.mobileController.text =
+                          controller.foundVendor[index].mobile!;
+
+                      _buildAddEditVendorView(
+                          text: 'UPDATE',
+                          addEditFlag: 2,
+                          docId: controller.foundVendor[index].docId!);
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: ElevatedButton(
+          onPressed: () {
+            _buildAddEditVendorView(text: 'ADD', addEditFlag: 1, docId: '');
+          },
+          child: Text('Add Vendor')),
+    );
+  }
+
+  _buildAddEditVendorView({String? text, int? addEditFlag, String? docId}) {
+    Get.bottomSheet(
+      Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(16.r),
+              topLeft: Radius.circular(16.r),
+            ),
+            color: Colors.blue.shade200),
+        child: Padding(
+          padding:
+              EdgeInsets.only(left: 16.w, right: 16.w, top: 16.h, bottom: 16.h),
+          child: Form(
+            key: controller.formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${text} Vendor',
+                    style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                    ),
+                    controller: controller.nameController,
+                    // validator: (value) {
+                    //   return controller.validateName(value!);
+                    // },
+                  ),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      hintText: 'address',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                    ),
+                    controller: controller.addressController,
+                  ),
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      hintText: 'Mobile',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                    ),
+                    controller: controller.mobileController,
+                  ),
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints.tightFor(
+                        width: Get.context!.width, height: 45.h),
+                    child: ElevatedButton(
+                      child: Text(
+                        text!,
+                        style: TextStyle(color: Colors.black, fontSize: 16.sp),
+                      ),
+                      onPressed: () {
+                        controller.saveUpdateVendor(
+                          controller.nameController.text,
+                          controller.addressController.text,
+                          controller.mobileController.text,
+                          docId!,
+                          addEditFlag!,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
+    );
+  }
+
+  displayDeleteDialog(String docId) {
+    Get.defaultDialog(
+      title: "Delete Vendor",
+      titleStyle: TextStyle(fontSize: 20.sp),
+      middleText: 'Are you sure to delete Vendor ?',
+      textCancel: "Cancel",
+      textConfirm: "Confirm",
+      confirmTextColor: Colors.black,
+      onCancel: () {},
+      onConfirm: () {
+        controller.deleteData(docId);
+      },
     );
   }
 }
