a50c73f790409913e262ab2bd8b48192fc1bf2dc
refayet21 <refayet21@gmail.com>
2024-03-03 07:44:42 +0600

    email pdf generated

--- lib/presentation/douser/invoice/douser_invoice.screen.dart
+++ lib/presentation/douser/invoice/douser_invoice.screen.dart
@@ -3,6 +3,8 @@ import 'package:dpil/presentation/widgets/do_drawer.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:pdf/pdf.dart';
+import 'package:printing/printing.dart';
 
 import 'controllers/douser_invoice.controller.dart';
 
@@ -18,126 +20,41 @@ class DouserInvoiceScreen extends GetView<DouserInvoiceController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: DoDrawer(),
       appBar: AppBar(
-        title: const Text('DouserInvoiceScreen'),
-        centerTitle: true,
+        title: Text('Email Sender App'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Obx(
-                () => DropdownButtonFormField<VendorModel>(
-                  onChanged: (value) {
-                    selectedVendor = value;
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Select Vendor',
-                    border: OutlineInputBorder(),
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                  ),
-                  isExpanded: true,
-                  value: selectedVendor,
-                  items: homeController.vendors
-                      .map((vendor) => DropdownMenuItem<VendorModel>(
-                            value: vendor,
-                            child: Text(vendor.name ?? 'N/A'),
-                          ))
-                      .toList(),
-                ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: productNameController,
-                decoration: InputDecoration(
-                  hintText: 'Product Name',
-                  border: OutlineInputBorder(),
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: productPriceController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: 'Product Price',
-                  border: OutlineInputBorder(),
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: productQuantityController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: 'Product Quantity',
-                  border: OutlineInputBorder(),
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                controller: addressController,
-                decoration: InputDecoration(
-                  hintText: 'Address',
-                  border: OutlineInputBorder(),
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                // onPressed: () {
-                //   String vendorInfo =
-                //       'Vendor Name: ${selectedVendor?.name ?? "N/A"}';
-                //   String dateInfo = 'Date: ${dateController.text}';
-                //   String productInfo =
-                //       'Product Name: ${productNameController.text}';
-                //   String priceInfo = 'Price: ${productPriceController.text}';
-                //   String quantityInfo =
-                //       'Quantity: ${productQuantityController.text}';
-                //   String addressInfo = 'Address: ${addressController.text}';
-
-                //   print(
-                //     '$vendorInfo\n$dateInfo\n$productInfo\n$priceInfo\n$quantityInfo\n$addressInfo',
-                //   );
-
-                //   homeController.generateInvoicePDF(vendorInfo);
-                // },
-                onPressed: () async {
-                  // Get the selected vendor, date, product details from the controllers
-                  String vendorInfo =
-                      'Vendor Name: ${selectedVendor?.name ?? "N/A"}';
-                  final date = dateController.text;
-                  final productName = productNameController.text;
-                  final productPrice =
-                      double.parse(productPriceController.text);
-                  final productQuantity =
-                      int.parse(productQuantityController.text);
-
-                  // Call the generateInvoicePDF method
-                  homeController.generateInvoicePdf(vendorInfo, date,
-                      productName, productPrice, productQuantity);
-                },
-                child: Text('Invoice'),
-              ),
-            ],
-          ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: controller.generatePdf,
+              child: Text('Generate PDF'),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Obx(() {
+                return controller.pdfBytes.value != null
+                    ? PdfPreview(
+                        build: (PdfPageFormat format) =>
+                            controller.pdfBytes.value!,
+                        initialPageFormat: PdfPageFormat.a4,
+                        allowPrinting: false,
+                        onPrinted: (_) {},
+                      )
+                    : Container();
+              }),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                controller
+                    .sendEmail(['refayet21@gmail.com'], 'subject', 'body');
+              },
+              child: Text('Send Email with PDF Attachment'),
+            ),
+          ],
         ),
       ),
     );
--- lib/presentation/douser/invoicepreview/douser_invoicepreview.screen.dart
+++ lib/presentation/douser/invoicepreview/douser_invoicepreview.screen.dart
@@ -3,34 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'controllers/douser_invoicepreview.controller.dart';
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
-import 'package:printing/printing.dart';
 
 class DouserInvoicepreviewScreen
     extends GetView<DouserInvoicepreviewController> {
-  final pw.Document? doc;
-  const DouserInvoicepreviewScreen({
-    Key? key,
-    this.doc,
-  }) : super(key: key);
+  const DouserInvoicepreviewScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          onPressed: () => Get.back(),
-          icon: Icon(Icons.arrow_back_outlined),
-        ),
+        title: const Text('DouserInvoicepreviewScreen'),
         centerTitle: true,
-        title: Text("Preview"),
       ),
-      body: PdfPreview(
-        build: (format) => doc!.save(),
-        allowSharing: true,
-        allowPrinting: true,
-        initialPageFormat: PdfPageFormat.roll80,
-        pdfFileName: "mydoc.pdf",
+      body: const Center(
+        child: Text(
+          'DouserInvoicepreviewScreen is working',
+          style: TextStyle(fontSize: 20),
+        ),
       ),
     );
   }
