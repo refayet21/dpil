05cd61ed360a43c6ccdb478e07edcde03a948f2f
refayet21 <refayet21@gmail.com>
2024-03-26 16:55:00 +0600

    do list show

--- lib/presentation/douser/invoice/controllers/douser_invoice.controller.dart
+++ lib/presentation/douser/invoice/controllers/douser_invoice.controller.dart
@@ -1,9 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class DouserInvoiceController extends GetxController {
+  final box = GetStorage();
+  RxList<Map<String, dynamic>> dousers = RxList<Map<String, dynamic>>();
+
   @override
   void onInit() {
     super.onInit();
+    getUserDo();
   }
 
   @override
@@ -14,5 +20,21 @@ class DouserInvoiceController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  Future<void> getUserDo() async {
+    final QuerySnapshot<Map<String, dynamic>> questionsQuery =
+        await FirebaseFirestore.instance
+            .collection("do_users")
+            .doc(box.read('employeeId'))
+            .collection('deliveryOrders')
+            .get();
+    dousers.value = questionsQuery.docs.map((doc) => doc.data()).toList();
+    print('dousers is $dousers');
+  }
+
+  // Getter method to retrieve dousers
+  RxList<Map<String, dynamic>> getDousers() {
+    return dousers;
   }
 }
--- lib/presentation/douser/invoice/douser_invoice.screen.dart
+++ lib/presentation/douser/invoice/douser_invoice.screen.dart
@@ -1,6 +1,6 @@
-import 'package:dpil/model/vendor.dart';
 import 'package:dpil/presentation/widgets/do_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'controllers/douser_invoice.controller.dart';
@@ -10,6 +10,64 @@ class DouserInvoiceScreen extends GetView<DouserInvoiceController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      drawer: DoDrawer(),
+      appBar: AppBar(
+        title: Text(
+          'DPIL',
+          style: TextStyle(fontSize: 30.sp, fontWeight: FontWeight.w700),
+        ),
+        centerTitle: true,
+      ),
+      body: Obx(() {
+        if (controller.dousers.isEmpty) {
+          // Show loading indicator if data is being fetched
+          if (controller.dousers.isEmpty && !controller.dousers.isNotEmpty) {
+            return Center(child: CircularProgressIndicator());
+          }
+          // Show message if data is empty
+          return Center(child: Text('No delivery orders found'));
+        } else {
+          // Display the list of doNo values using ListView.builder
+          return ListView.builder(
+            itemCount: controller.dousers.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  title: Text('Do No: ${controller.dousers[index]['doNo']}'),
+                  subtitle: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 3.h,
+                      ),
+                      Text(
+                        'Customer Name :${controller.dousers[index]['vendorName']}',
+                        style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black),
+                      ),
+                      SizedBox(
+                        height: 3.h,
+                      ),
+                      Text(
+                        'Delivery Date : ${controller.dousers[index]['deliveryDate']}',
+                        style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black),
+                      ),
+                    ],
+                  ),
+                  onTap: () {},
+                ),
+              );
+            },
+          );
+        }
+      }),
+    );
   }
 }
